6a025e5e0e08fa421a4d741904ecbcecf1d6f258
Artem Getman <a.e.getman@gmail.com>
2024-05-19 14:26:40 +0300

    All tasks

---
+++ task_4.py
@@ -0,0 +1,37 @@
+class Comment:
+    def __init__(self, text, author):
+        self.text = text
+        self.author = author
+        self.replies = []
+        self.is_deleted = False
+
+    def add_reply(self, reply):
+        self.replies.append(reply)
+
+    def remove_reply(self):
+        self.text = "Цей коментар було видалено."
+        self.is_deleted = True
+
+    def display(self, level=0):
+        if not self.is_deleted:
+            print("\t" * level + f"{self.author}: {self.text}")
+        else:
+            print("\t" * level + self.text)
+        
+        for reply in self.replies:
+            reply.display(level + 1)
+
+# Приклад використання:
+root_comment = Comment("Яка чудова книга!", "Бодя")
+reply1 = Comment("Книга повне розчарування :(", "Андрій")
+reply2 = Comment("Що в ній чудового?", "Марина")
+
+root_comment.add_reply(reply1)
+root_comment.add_reply(reply2)
+
+reply1_1 = Comment("Не книжка, а перевели купу паперу ні нащо...", "Сергій")
+reply1.add_reply(reply1_1)
+
+reply1.remove_reply()
+
+root_comment.display()
